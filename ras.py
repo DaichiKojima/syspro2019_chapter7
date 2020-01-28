bcbb88278d8244243bfe94de93a4ff3db88d9467
DaichiKojima <kojimadaichi0@gmail.com>
2020-01-28 09:45:15 +0900

    s16077LED課題提出

---
+++ ras.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import time
+import RPi.GPIO as GPIO
+import smbus
+import datetime
+import json
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/pi/syspro-chapter8.json"
+cred = credentials.Certificate('/home/pi/syspro-chapter8.json')
+firebase_admin.initialize_app(cred)
+i2c = smbus.SMBus(1)
+address = 0x48
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.output(14, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(14, GPIO.LOW)
+db = firestore.Client()
+
+def on_snapshot(doc_snapshot, changes, read_time):
+    for change in changes:
+        print(u'New cmd: {}'.format(change.document.id))
+        led = change.document.to_dict()["led"]
+        print(u'LED: {}'.format(led))
+        if led == "ON":
+            print "ON"
+            GPIO.output(14, GPIO.HIGH)
+
+
+        elif led == "OFF":
+            print "OFF"
+            GPIO.output(14, GPIO.LOW)
+
+
+
+on_ref = db.collection('led').where(u'led', u'==', u'ON')
+off_ref = db.collection('led').where(u'led', u'==', u'OFF')
+doc_watch = on_ref.on_snapshot(on_snapshot)
+doc_watch = off_ref.on_snapshot(on_snapshot)
+
+'''
+while True:
+    #温度，湿度，気圧の値を取得する
+    #temp = 
+    #hum = 
+    #press = 
+    print("Temperature:%6.2f" %(temp))
+    print("Humidity:%6.2f" %(hum))
+    print("Pressure:%6.2f" %(press))
+    data = {"temp": temp, "hum": hum, "press":press}
+    db.collection('temperature').document(str(datetime.datetime.now())).set(data)
+    time.sleep(1)
+'''
+# 温度センサと接続できないうちはこの無限ループを使う
+while True:
+    pass
